9a5aecd5608b1d2235e17da6a2e3320739502149
guncel-aldemir <guncelaldemir@gmail.com>
2023-07-02 14:32:12 +0300

    Initial Commit

---
+++ Example-MVVM-APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example-MVVM-APP
+//
+//  Created by guncel on 2.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
